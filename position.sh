253127676b6f5c3af60916f5def1ddafea858502
Laurence Willetts <wildefur@gmail.com>
2015-11-05 13:33:46 +0000

    position.sh, update rht case

--- position.sh
+++ position.sh
@@ -60,7 +60,7 @@ case $1 in
         SW=$((SW - 2*XGAP - BW))
         H=$((SH - TGAP - BGAP - BW))
         W=$((SW/2 - IGAP/2 - BW))
-        X=$((W + XGAP + IGAP - BW))
+        X=$((W + XGAP + IGAP))
         ;;
     ext)
         Y=$TGAP
