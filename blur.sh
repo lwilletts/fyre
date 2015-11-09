c6eaef44882a82a452e864dfeb3f76f23cc86f26
Laurence Willetts <wildefur@gmail.com>
2015-11-09 13:02:34 +0000

    blur.sh, move BLUR to fyrerc

--- blur.sh
+++ blur.sh
@@ -3,7 +3,8 @@
 # wildefyr - 2015 (c) wtfpl
 # blur current background
 
-BLUR=20
+source ~/.config/fyre/fyrerc
+
 WALL=$HOME$(cat $(which bgc) | cut -d~ -f 2)
 
 hsetroot -tile $WALL -blur ${1:-$BLUR}
