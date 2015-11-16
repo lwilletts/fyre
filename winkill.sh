a889a7b7d65acdb1b747fae5cf772d6cd84150ac
Laurence Willetts <wildefur@gmail.com>
2015-11-16 15:10:32 +0000

    winkill.sh, use killwa to safely kill urxvtd terminals.

--- winkill.sh
+++ winkill.sh
@@ -4,14 +4,19 @@
 # performs checks when window has been targetted for termination
 
 wid=$(pfw)
+windowC=$(wclass.sh c $wid)
 windowM=$(wclass.sh m $wid)
 
-if [ $(deletelock.sh status $wid) -eq 1 ]; then
-    exit
+if [[ $windowC == "Terminal" ]]; then
+    killwa $wid
 fi
 
 if [[ $windowM == "Firefox" ]]; then
     killwa $wid
+fi
+
+if [ $(deletelock.sh status $wid) -eq 1 ]; then
+    exit
 fi
 
 printf '%s\n' "you're terminated fucker."
