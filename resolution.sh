7f6da9ec3e5da5053f1adc996dc30a99501cf92e
Laurence Willetts <wildefur@gmail.com>
2015-11-21 21:44:21 +0000

    resolution.sh: remove awk method

--- resolution.sh
+++ resolution.sh
@@ -15,7 +15,7 @@ fi
 wid=$1
 
 if [[ $(wclass.sh m $wid) == "mpv" ]]; then
-    printf '%s\n' "$(xprop -id $wid WM_NORMAL_HINTS | awk '/maximum/ {print $NF}' | sed 's#/# #')"
+    printf '%s\n' "$(xprop -id $(wid.sh mpv) WM_NORMAL_HINTS | sed '5s/[^0-9]*//p;d' | sed 's#/# #')"
 else
     usage
 fi
