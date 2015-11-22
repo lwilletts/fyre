e6c672a3c242f22ec1d401d24b3ab68317254746
Laurence Willetts <wildefur@gmail.com>
2015-11-22 06:38:26 +0000

    add: trans.sh, transset-df wrapper for sxhkd

---
+++ trans.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+#
+# wildefyr - 2015 (c) wtfpl
+# transset-df wrapper for sxhkd
+
+usage() {
+    printf '%s\n' "usage: $(basename $0) <wid> <value> (inc|dec by value)"
+    exit 1
+}
+
+# test for wid value
+case $1 in 
+    0x*) wid=$1 ;;
+    *) usage ;;
+esac
+
+# test for number
+case $2 in
+    ''|*[0-9]*) opacity=$2 ;;
+    *) usage ;;
+esac
+
+# test for last argument
+case $3 in
+    u|inc) transset-df -i $wid --inc $opacity; exit ;;
+    d|dec) transset-df -i $wid --dec $opacity; exit ;;
+    *) transset-df -i $wid $opacity ;;
+esac
+
