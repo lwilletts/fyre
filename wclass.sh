93fc7d7c9f7ca178ec529ac0ec9c295664e3a25d
Laurence Willetts <wildefur@gmail.com>
2015-11-23 10:18:06 +0000

    testing lswargs

--- wclass.sh
+++ wclass.sh
@@ -4,12 +4,14 @@
 # checks a windows class
 
 usage() {
-    printf '%s\n' "usage: $(basename $0) <command> <wid>" >&2
+    printf '%s\n' "usage: $(basename $0) <command> (wid|unlisted)"
     exit 1
 }
 
 case $2 in
-    u|-u|unlisted) hidden="-u" ;;
+    u|-u|unlisted) lswArgs="-u" ;;
+    o|-o|override) lswArgs="-o" ;;
+    a|-a|all) lswArgs="-a" ;;
 esac
 
 case $1 in
@@ -20,27 +22,27 @@ case $1 in
         xprop -id $2 WM_CLASS | cut -f 4 -d \"
         ;;
     ca|classAll)
-        for i in $(seq $(lsw $hidden | wc -l)); do
-            wid=$(lsw $hidden | head -n $i | tail -1)
+        for i in $(seq $(lsw $lswArgs | wc -l)); do
+            wid=$(lsw $lswArgs | head -n $i | tail -1)
             printf '%s\n' "$wid $(wclass.sh c $wid)"
         done
         ;;
     ma|moreAll)
-        for i in $(seq $(lsw $hidden | wc -l)); do
-            wid=$(lsw $hidden | head -n $i | tail -1)
+        for i in $(seq $(lsw $lswArgs | wc -l)); do
+            wid=$(lsw $lswArgs | head -n $i | tail -1)
             printf '%s\n' "$wid $(wclass.sh m $wid)"
         done
         ;;
     name)
-        for i in $(seq $(lsw $hidden | wc -l)); do
-            wid=$(lsw $hidden | head -n $i | tail -1)
+        for i in $(seq $(lsw $lswArgs | wc -l)); do
+            wid=$(lsw $lswArgs | head -n $i | tail -1)
             printf '%s\n' "$wid $(wname $wid)"
         done
         ;;
     all)
-        $(basename $0) ca
-        $(basename $0) ma
-        $(basename $0) name
+        $(basename $0) ca $lswArgs
+        $(basename $0) ma $lswArgs
+        $(basename $0) name $lswArgs
         ;;
     *)
         usage
--- wid.sh
+++ wid.sh
@@ -1,14 +1,22 @@
-#!/bin/sh
+#bin/sh
 #
 # wildefyr - 2015 (c) wtfpl
 # wrapper for wclass.sh to find any wid's that match a string
+# much easier to type
 
 usage() { 
-    printf '%s\n' "usage: $(basename $0) <string>"
+    printf '%s\n' "usage: $(basename $0) <string> (unlisted)"
+    exit 1
 }
 
 if [ -z $1 ]; then
     usage
 fi
 
-wclass.sh all | grep -i $1 | cut -d\  -f 1 | uniq
+case $1 in
+    u|-u|unlisted) lswArgs="-u" ;;
+    o|-o|override) lswArgs="-o" ;;
+    a|-a|all) lswArgs="-a" ;;
+esac
+
+wclass.sh all $lswArgs # | grep -i $1 | cut -d\  -f 1 | uniq
