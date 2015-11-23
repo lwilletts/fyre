3dd2e52d2f695698cb1d3fc6ad49582fdda01f1a
Laurence Willetts <wildefur@gmail.com>
2015-11-23 21:44:00 +0000

    winopen.sh: use global variable for transset-df

--- winopen.sh
+++ winopen.sh
@@ -3,6 +3,8 @@
 # wildefyr - 2015 (c) wtfpl
 # test window classes when being opened
 
+. ~/.config/fyre/fyrerc
+
 usage() {
     printf '%s\n' "usage: $(basename $0) <wid>"
     exit 1
@@ -26,6 +28,7 @@ elif [ "$windowC" = "Dialog" ]; then
     position.sh md $wid
 elif [ "$windowC" = "mosh" ]; then
     position.sh mid $wid
+    transset-df -i $wid $TRANS
     wgroups.sh -s $wid 3
 elif [ "$windowC" = *"ts3"* ]; then
     position.sh tl $wid
@@ -39,16 +42,21 @@ elif [ "$windowC" = "ncmpcpp" ]; then
     position.sh tr $wid
     position.sh ext $wid
     wgroups.sh -s $wid 8
+    transset-df -i $wid $TRANS
 elif [ "$windowC" = "mpsyt" ]; then
     position.sh tr $wid
     position.sh ext $wid
     wgroups.sh -s $wid 9
+    transset-df -i $wid $TRANS
 elif [ "$windowC" = "alsamixer" ]; then
     position.sh mid $wid
+    transset-df -i $wid $TRANS
 elif [ "$windowC" = "Terminal" ]; then
+    transset-df -i $wid $TRANS
     tile.sh
 elif [ "$windowC" = "urxvt" ]; then
     position.sh md $wid
+    transset-df -i $wid $TRANS
 else
     position.sh md $wid
 fi
