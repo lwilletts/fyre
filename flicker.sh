fe8f780381d7a658c818630c5ac11d3f8a102948
Laurence Willetts <wildefur@gmail.com>
2015-11-26 09:38:03 +0000

    flicker.sh, add new values

--- flicker.sh
+++ flicker.sh
@@ -2,9 +2,11 @@
 #
 # wildefyr - 2015 (c) wtfpl
 # make the current window 'flicker' - inspired by rainbow.sh
+# this doesn't work too well on transparent windows that overlap
 
 FREQ=${FREQ:-0.05}
-VALUES="0.9 0.89 0.88 0.87 0.86 0.85 0.84 0.83 0.82 0.81 0.80 0.81 0.82 0.83 0.84 0.85 0.86 0.87 0.88 0.89 0.9" 
+VALUES="0.9 0.89 0.88 0.87 0.86 0.85 0.84 0.83 0.82 0.81 0.80 0.79 0.78 0.77
+0.76 0.75 0.76 0.77 0.78 0.79 0.80 0.81 0.82 0.83 0.84 0.85 0.86 0.87 0.88 0.89 0.9" 
 
 TRANS=$(echo $VALUES | cut -d\  -f 1)
 
