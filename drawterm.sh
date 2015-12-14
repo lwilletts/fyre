80d20154f1c767b8d2a92c5249f23855fc0bdb8b
Laurence Willetts <wildefur@gmail.com>
2015-12-14 11:28:53 +0000

    drawterm.sh: add checks and slop arguments

--- drawterm.sh
+++ drawterm.sh
@@ -3,6 +3,13 @@
 # wildefyr - 2015 (c) wtfpl
 # use slop to get xywh position for urxvt
 
-eval $(slop)
+. ~/.config/fyre/fyrerc
+
+eval $(slop -t 0 -b $BW '215,215,215,0.9')
+
+if [ $ID -eq 0 ]; then
+    exit
+fi
+
 urxvtc -name 'slop'
 wtp $X $Y $W $H $(wid.sh "slop" | tail -1)
