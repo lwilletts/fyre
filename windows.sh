63d913c2a4fed6b0af6195e521123c4e06402c7c
Laurence Willetts <wildefur@gmail.com>
2015-12-14 10:56:40 +0000

    windows.sh: minor syntax updates

--- windows.sh
+++ windows.sh
@@ -1,4 +1,4 @@
-#!/bin/dash
+#!/bin/sh
 #
 # wildefyr - 2015 (c) wtfpl
 # wrote my own groups script because Greduan's groups.sh was pissing me off
@@ -6,29 +6,36 @@
 . ~/.config/fyre/fyrerc
 
 usage() {
-    printf '%s\n' "usage: $(basename $0) <add|hide|show|toggle|clean|reset|list> (group) (pfw)"
+    printf '%s\n' "usage: $(basename $0) <a|h|s|t|c|r|l> (group) (pfw)"
     exit 1
 }
 
 add_group() {
+    echo 1
 }
 
 hide_group() {
+    echo 1
 }
 
 show_group() {
+    echo 1
 }
 
 toggle_group() {
+    echo 1
 }
 
 clean_group() {
+    echo 1
 }
 
 reset_groups() {
+    echo 1
 }
 
 list_groups() {
+    vcat $GROUPSDIR/*.*
 }
 
 if [ $# -eq 0 ]; then
@@ -36,18 +43,12 @@ if [ $# -eq 0 ]; then
 fi
 
 case $1 in 
-    a|add)
-        add_group ;;
-    h|hide)
-        hide_group ;;
-    s|show)
-        show_group ;;
-    t|toggle)
-        toggle_group ;;
-    c|clean)
-        clean_group ;;
-    r|reset)
-        reset_groups ;;
-    l|list)
-        list_groups ;;
+    a|add) add_group ;;
+    h|hide) hide_group ;;
+    s|show) show_group ;;
+    t|toggle) toggle_group ;;
+    c|clean) clean_group ;;
+    r|reset) reset_groups ;;
+    l|list) list_groups ;;
+    *) usage ;;
 esac
