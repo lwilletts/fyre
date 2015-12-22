15fe537d00a5c3344e593a53625a5f33c1119903
Laurence Willetts <wildefur@gmail.com>
2015-12-22 05:35:17 +0000

    snap.sh: snap now handles any movement of a window where it's size is not changed

--- snap.sh
+++ snap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 # wildefyr & kekler - 2015 (c) wtfpl
-# snap windows to borders - fyre compatible
+# snap windows to common positions - fyre compatible
 
 . ~/.config/fyre/fyrerc
 
@@ -26,10 +26,35 @@ snap_right() {
     wtp $((SW - XGAP - W - 2*BW)) $(wattr y $PFW) $W $H $PFW
 }
 
+snap_tl() {
+    wtp $X $TGAP $W $H $PFW
+}
+
+snap_tr() {
+    wtp $((SW - XGAP - W - 2*BW)) $TGAP $W $H $PFW
+}
+
+snap_bl() {
+    wtp $X $((SH - BGAP - H)) $W $H $PFW
+}
+
+snap_br() {
+    wtp $((SW - XGAP - W - 2*BW)) $((SH - BGAP - H)) $W $H $PFW
+}
+
+snap_md() {
+    wtp $((SW/2 - W/2 - BW)) $((SH/2 - H/2 - BW)) $W $H $PFW
+}
+
 case $1 in 
-    h|left)  snap_left ;;
-    j|down)  snap_down ;;
-    k|up)    snap_up ;;
+    h|left)  snap_left  ;;
+    j|down)  snap_down  ;;
+    k|up)    snap_up    ;;
     l|right) snap_right ;;
-    *)       usage ;;
+    tl)      snap_tl    ;;
+    tr)      snap_tr    ;;
+    bl)      snap_bl    ;;
+    br)      snap_br    ;;
+    md)      snap_md    ;; 
+    *)       usage      ;;
 esac
