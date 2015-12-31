78d8662d3f8426c51d8b4b12dbdd0d839def1df8
Laurence Willetts <wildefur@gmail.com>
2015-12-31 18:35:17 +0000

    resolution.sh: update error msg and exit code

--- resolution.sh
+++ resolution.sh
@@ -4,7 +4,7 @@
 # find resoluton of mpv video based on window id
 
 usage() {
-    printf '%s\n' "usage: $(basename $0) <mpvwid>"
+    printf '%s\n' "usage: $(basename $0) <wid>"
     exit 1
 }
 
@@ -15,5 +15,6 @@ wid=$1
 if [ $(wclass.sh c $wid) = "mpv" ]; then
     printf '%s\n' "$(xprop -id $wid WM_NORMAL_HINTS | sed '5s/[^0-9]*//p;d' | tr / \ )"
 else
-    usage
+    printf '%s\n' "error: not a mpv wid"
+    exit 2
 fi
