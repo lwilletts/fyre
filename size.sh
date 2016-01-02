a56e98fbc9596a2145ecb9733545a01bd814ab8d
Laurence Willetts <wildefur@gmail.com>
2016-01-02 13:26:15 +0000

    Add: size.sh

---
+++ size.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+#
+# wildefyr - 2015 (c) wtfpl
+# sane resizing in a direction
+
+. ~/.config/fyre/fyrerc
+
+usage() {
+    printf '%s\n' "usage: $(basename $0) <gd|gr|sl|su> (wid)"
+    exit 1
+}
+
+case $2 in
+    0x*) PFW=$2 ;;
+esac
+
+grow_down() {
+    wtp $X $Y $W $((H*2 + VGAP + BW)) $PFW
+}
+
+grow_right() {
+    wtp $X $Y $((W*2 + IGAP + BW)) $H $PFW
+}
+
+shrink_left() {
+    wtp $X $Y $((W/2 - BW)) $H $PFW
+}
+
+shrink_up() {
+    wtp $X $Y $W $((H/2 - BW)) $PFW
+}
+
+case $1 in 
+    gl|growleft)    grow_left    ;;
+    gd|growdown)    grow_down    ;;
+    gu|growup)      grow_up      ;;
+    gr|growright)   grow_right   ;;
+    sl|shrinkleft)  shrink_left  ;;
+    sd|shrinkdown)  shrink_down  ;;
+    su|shrinkup)    shrink_up    ;;
+    sr|shrinkright) shrink_right ;;
+esac
