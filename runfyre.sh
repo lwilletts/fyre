772af725f99f11b41100a1649ce1f82d5fe0dc95
Laurence Willetts <wildefur@gmail.com>
2016-01-07 17:16:53 +0000

    runfyre.sh: save mpv position

--- runfyre.sh
+++ runfyre.sh
@@ -1,8 +1,25 @@
 #!/bin/sh
 #
-# wildefyr - 2015 (c) wtfpl
-# infinite loops are the best loops
+# wildefyr - 2016 (c) wtfpl
+# checks currently visible windows
 
-while true; do
-    sleep 1
+. ~/.config/fyre/fyrerc
+
+DURATION=20
+test ! -z $1 && DURATION=$1
+
+while :; do
+
+    for wid in $(lsw); do
+        windowC=$(wclass.sh c $wid)
+
+        if [ $windowC = "mpv" ]; then
+            printf '%s ' $(wattr xywh $wid) > $MPVPOS
+        elif [ $windowC = "mpv" ] && [ ! -f $MPVPOS ]; then
+            rm $MPVPOS
+        fi
+
+    done
+
+    sleep $DURATION
 done
