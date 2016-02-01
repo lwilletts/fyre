f7d8e5149b1bd55bfb8f7082b5e2cce4e83caa95
Laurence Willetts <wildefur@gmail.com>
2016-02-01 13:30:04 +0000

    drawterm.sh: refactor

--- drawterm.sh
+++ drawterm.sh
@@ -5,45 +5,25 @@
 
 ARGS="$@"
 
-usage() {
-    cat << EOF
-Usage: $(basename $0) ARGUMENTS
-    c | cmd:  Terminal command to run.
-    h | help: Show this help.
-EOF
-
-    test -z $1 || exit $1
-}
-
 main() {
     . fyrerc.sh
 
-    for arg in "$@"; do
-        case $arg in
-            -c|--cmd)   COMMANDFLAG=true ;;
-            -h|--help)  usage 0          ;;
-        esac
-
-        case $arg in
-            -*)  continue ;;
-            --*) continue ;;
-        esac
-
-        test "$COMMANDFLAG" = "true" && {
-            CMDSTRING="$CMDSTRING $arg"
-        }
-    done
-
     eval $(slop -t 0 -b $BW '215,215,215,0.9')
 
-    test ! -z "$CMDSTRING" && {
-        urxvt -name 'slop' -e zsh -c "$CMDSTRING" &
+    test ! -z "$1" && {
+        urxvt -e sh -c "$@" &
     } || {
         urxvt -name 'slop' &
     }
 
-    sleep 0.05
-    wtp $X $Y $W $H $(wid.sh "slop" | tail -1)
+    sleep 0.01
+    slopWid=$(wid.sh "slop" | tail -1)
+
+    test ! -z $slopWid && {
+        wtp $X $Y $W $H $slopWid
+    } || {
+        printf '%s\n' "URxvt window was not found." >&2
+    }
 }
 
 main $ARGS
