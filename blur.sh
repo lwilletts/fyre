3fafc3a839321fcf11b3b8f6199a15a84579ac98
Laurence Willetts <wildefur@gmail.com>
2016-02-09 15:31:07 +0000

    blur.sh: small refactor

--- blur.sh
+++ blur.sh
@@ -10,6 +10,14 @@ usage() {
     test -z $1 || exit $1
 }
 
+intCheck() {
+    test $1 -ne 0 2> /dev/null
+    test $? -ne 2 || {
+         printf '%s\n' "'$1' is not an integer." >&2
+         exit 1
+    }
+}
+
 main() {
     . fyrerc.sh
 
@@ -17,13 +25,7 @@ main() {
         h|help) usage 0 ;;
     esac
 
-    test "$1" != "" && {
-        test $1 -ne 0 > /dev/null
-        test $? -ne 2 || {
-            printf '%s\n' "$1 is not an integer." >&2
-            exit 1
-        }
-    }
+    test -z $1 || intCheck $1
 
     $WALL -blur ${1:-$BLUR}
 }
