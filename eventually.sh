f2c5aa23e01af2063d116cae35373f3ca408c332
Laurence Willetts <wildefur@gmail.com>
2016-02-28 04:37:12 +0000

    eventually.sh: cleanup

--- eventually.sh
+++ eventually.sh
@@ -24,18 +24,14 @@ wew | while IFS=: read ev wid; do
             ;;
         18)
             wattr o "$wid" || {
-                wattr "$(pfw)" || {
-                    focus.sh prev 2>/dev/null
-                }
-
+                wattr "$(pfw)" || focus.sh prev "disable" 2>/dev/null
                 setborder.sh inactive "$wid"
                 test "$(lsw | wc -l)" -eq 0 && blur.sh 0
             }
             ;;
         19)
             wattr o "$wid" || {
-                chwso -r "$wid"
-                chwso -r "$(pfw)"
+                focus.sh "$wid" "disable"
                 test "$(lsw | wc -l)" -ne 0 && blur.sh
             }
             ;;
