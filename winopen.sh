691637be3b554c988fb92126f630f4f8bc98f304
Laurence Willetts <wildefur@gmail.com>
2016-02-29 05:20:55 +0000

    winopen.sh: improve syntax

--- winopen.sh
+++ winopen.sh
@@ -7,10 +7,10 @@ ARGS="$@"
 
 usage() {
     cat << EOF
-Usage: $(basename $0) <wid>
+Usage: $(basename $0) [wid]
 EOF
 
-    test -z $1 || exit $1
+    test $# -eq 0 || exit $1
 }
 
 main() {
@@ -24,81 +24,67 @@ main() {
     windowName=$(name $wid)
     windowClass=$(class $wid)
 
-    # for speed
     setborder.sh active $wid
 
-    # put the things you open the most often at the top
-    test "$windowName" = "urxvt" && {
-        position.sh res $wid
-        snap.sh tl $wid
-        exit
-    }
+    case "$windowName" in
+        "urxvt")
+            position.sh res $wid
+            snap.sh tl $wid
+            ;;
+        "mosh")
+            position.sh res $wid
+            position.sh ext $wid
+            snap.sh tl $wid
+            wgroups.sh -s $wid 3
+            ;;
+        "tmux")
+            position.sh res $wid
+            position.sh ext $wid
+            snap.sh tl $wid
+            wgroups.sh -s $wid 1
+            ;;
+        "mpsyt")
+            position.sh res $wid
+            position.sh ext $wid
+            snap.sh right $wid
+            wgroups.sh -s $wid 9
+            ;;
+        "alsamixer")
+            position.sh quar $wid
+            snap.sh md $wid
+            ;;
+        "Legions.exe")
+            wgroups.sh -s $wid 6
+            ;;
+        *)
+            case "$windowClass" in
+                "google-chrome")
+                    wtp $(($(wattr x $wid) - BW)) $(($(wattr y $wid) - BW)) \
+                        $(wattr wh $wid) $wid
+                    wgroups.sh -s $wid 2
+                    ;;
+                "mpv")
+                    wgroups.sh -s $wid 5
+                    ;;
+                "TeamSpeak 3")
+                    snap.sh md $wid
+                    wgroups.sh -s $wid 4
+                    ;;
+                # seriously fuck this program
+                "telegram")
+                    wgroups.sh -s $wid 4
+                    ;;
+                "MuPDF")
+                    snap.sh md $wid
+                    wgroups.sh -s $wid 6
+                    ;;
+                *)
+                    snap.sh md $wid
+                    ;;
+            esac
+            ;;
+    esac
 
-    test "$windowClass" = "google-chrome" && {
-        wtp $(($(wattr x $wid) - BW)) $(($(wattr y $wid) - BW)) $(wattr wh $wid) $wid
-        wgroups.sh -s $wid 2
-        exit
-    }
-
-    test "$windowName" = "mosh" && {
-        position.sh res $wid
-        position.sh ext $wid
-        snap.sh tl $wid
-        wgroups.sh -s $wid 3
-        exit
-    }
-
-    test "$windowName" = "tmux" && {
-        position.sh res $wid
-        position.sh ext $wid
-        snap.sh tl $wid
-        wgroups.sh -s $wid 1
-        exit
-    }
-
-    test "$windowName" = "mpsyt" && {
-        position.sh res $wid
-        position.sh ext $wid
-        snap.sh right $wid
-        wgroups.sh -s $wid 9
-        exit
-    }
-
-    test "$windowName" = "alsamixer" && {
-        position.sh quar $wid
-        snap.sh md $wid
-        exit
-    }
-
-    test "$windowClass" = "mpv" && {
-        wgroups.sh -s $wid 5
-        position.sh vid $wid
-        exit
-    }
-
-    test "$windowClass" = "TeamSpeak 3" && {
-        snap.sh md $wid
-        wgroups.sh -s $wid 4
-        exit
-    }
-
-    test "$windowClass" = "telegram" && {
-        snap.sh md $wid
-        wgroups.sh -s $wid 4
-        exit
-    }
-
-    test "$windowClass" = "MuPDF" && {
-        wgroups.sh -s $wid 6
-        exit
-    }
-
-    test "$windowName" = "Legions.exe" && {
-        wgroups.sh -s $wid 6
-        exit
-    }
-
-    snap.sh md $wid
 }
 
 main $ARGS
