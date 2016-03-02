9240566a10ff70f8e9f91002e73c0472ddd7042f
Laurence Willetts <wildefur@gmail.com>
2016-03-02 12:16:17 +0000

    Add: ign.sh

---
+++ ign.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#
+# wildefyr - 2016 (c) wtfpl
+# toggle for ignw and override_redirect
+
+usage() {
+    cat << EOF
+Usage: $(basename $0) [wid]
+EOF
+
+    test $# -eq 0 || exit $1
+}
+
+. fyrerc.sh
+
+case $1 in
+    0x*) PFW=$1  ;;
+    *)   usage 0 ;;
+esac
+
+wattr o "$PFW" && {
+    ignw -r "$PFW"
+    buffer=$(sed "s/$PFW//" < "$IGNORE")
+    printf '%s\n' "$buffer" | sed '/^\s*$/d' > "$IGNORE"
+    test -z "$(cat $IGNORE)" 2> /dev/null && {
+        rm -f $IGNORE
+    }
+} || {
+    ignw -s "$PFW"
+    printf '%s\n' "$PFW" >> "$IGNORE"
+}
