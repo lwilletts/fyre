e4445bd768b335151f418c77cdb42d989fda8597
Laurence Willetts <me@wildefyr.net>
2016-03-10 19:01:53 +0000

    winopen.sh: improve mpv opening further

--- winopen.sh
+++ winopen.sh
@@ -57,46 +57,53 @@ main() {
             windows.sh -a $wid 6
             ;;
         *)
-            case "$(class $wid)" in
-                "google-chrome")
-                    wtp $(($(wattr x $wid) - BW)) $(($(wattr y $wid) - BW)) \
-                        $(wattr wh $wid) $wid
-                    windows.sh -a $wid 2
-                    ;;
-                "mpv")
-                    windows.sh -a $wid 5
-
-                    test -f "$MPVPOS" && {
-                        wtp $(cut -d\  -f -4 < $MPVPOS) $wid
-                    } || {
-                        test $(resolution $wid | cut -d\  -f 1) -ge $SW && {
-                            position.sh full $wid
-                        }
-                        test $(resolution $wid | cut -d\  -f 2) -ge $SH && {
-                            position.sh full $wid
-                        }
-                        snap.sh md $wid
-                    }
-                    ;;
-                "TeamSpeak 3")
-                    snap.sh md $wid
-                    windows.sh -a $wid 4
-                    ;;
-                # seriously fuck this program
-                "telegram")
-                    position.sh res $wid
-                    position.sh ext $wid
-                    windows.sh -a $wid 4
-                    ;;
-                "MuPDF")
-                    snap.sh md $wid
-                    windows.sh -a $wid 6
-                    ;;
-                *)
-                    snap.sh md $wid
-                    ;;
-            esac
+    case "$(class $wid)" in
+        "google-chrome")
+            wtp $(($(wattr x $wid) - BW)) $(($(wattr y $wid) - BW)) \
+                $(wattr wh $wid) $wid
+            windows.sh -a $wid 2
             ;;
+        "mpv")
+            snap.sh md $wid
+            windows.sh -a $wid 5
+
+            test -f "$MPVPOS" && {
+                test -z "$(wid.sh mpv)" && {
+                    test ! -f "$FSFILE" && {
+                        wtp $(cut -d\  -f -4 < $MPVPOS) $wid
+                    }
+                }
+            } || {
+                test "$(resolution $wid | cut -d\  -f 1)" -ge $SW && {
+                    position.sh full $wid
+                }
+                test "$(resolution $wid | cut -d\  -f 2)" -ge $SH && {
+                    position.sh full $wid
+                }
+            }
+            ;;
+        "Steam")
+            windows.sh -a $wid 7
+            ;;
+        "TeamSpeak 3")
+            snap.sh md $wid
+            windows.sh -a $wid 4
+            ;;
+        # seriously fuck this program
+        "telegram")
+            position.sh res $wid
+            position.sh ext $wid
+            windows.sh -a $wid 4
+            ;;
+        "MuPDF")
+            snap.sh md $wid
+            windows.sh -a $wid 6
+            ;;
+        *)
+            snap.sh md $wid
+            ;;
+    esac
+    ;;
     esac
 }
 
