aea245abad0debe72872caec5fd874e3bbe1fc1c
Laurence Willetts <me@wildefyr.net>
2016-03-10 19:02:46 +0000

    taskbar.sh: improve syntax

--- taskbar.sh
+++ taskbar.sh
@@ -11,46 +11,49 @@ INACTIVE="%{F#737373}"
 
 for group in $GROUPSDIR/group.?; do
     title=""
-    numGroups=$(ls $GROUPSDIR/group.? | wc -l)
+    numGroups=$(find $GROUPSDIR/group.? 2> /dev/null | wc -l)
     groupNum=$(printf "$group" | rev | cut -d'.' -f 1 | rev)
 
     colour="$INACTIVE"
-    for groupId in $(cat "$GROUPSDIR/active"); do
+
+    while read -r groupId; do
         test "$groupId" -eq "$groupNum" && {
             colour="$ACTIVE"
             break
         } || {
             colour="$INACTIVE"
         }
-    done
+    done < $GROUPSDIR/active
 
-    wids=$(cat $group | tr '\n' ' ')
+    wids=$(tr '\n' ' ' < $group)
 
     for wid in $wids; do
         window="$(class ${wid})"
-
-        test "$window" = "URxvt" && {
-            window="$(name ${wid})"
-            test "$window" = "urxvt" && {
+        case "$window" in
+            "URxvt")
+                window="$(name ${wid})"
+                case "$window" in
+                    "urxvt")
+                        window="$(wname ${wid})"
+                        ;;
+                    "tmux")
+                        window="$(wname ${wid})"
+                        ;;
+                    "mosh")
+                        window="mosh: $(ps $(process $wid) | tail -1 | awk '{printf $NF}')"
+                        ;;
+                esac
+                ;;
+            "mpv")
+                window="$(wname ${wid}) - $(resolution ${wid} | cut -d\  -f 2)p"
+                ;;
+            "wine")
                 window="$(wname ${wid})"
-            }
-            test "$window" = "tmux" && {
-                window="$(wname ${wid})"
-            }
-            test "$window" = "mosh" && {
-                window="mosh: $(ps $(process $wid) | tail -1 | awk '{printf $NF}')"
-            }
-        }
-        test "$window" = "mpv" && {
-            window="$(wname ${wid}) - $(resolution ${wid} | cut -d\  -f 2)p"
-        }
-        test "$window" = "Wine" && {
-            window="$(wname ${wid})"
-        }
-
-        window=$(printf '%s' "$window" | sed '
-        s_google-chrome_chrome_
-        ')
+                ;;
+            "google-chrome")
+                window="chrome"
+                ;;
+        esac
 
         test -z "$title" && {
             title="$(printf "$window" | tr '\n' ' ')"
@@ -61,8 +64,10 @@ for group in $GROUPSDIR/group.?; do
 
     test ! -z "$title" && {
         stringOut="${stringOut}${colour}\
-%{A:windows.sh -t $groupNum:}  #${groupNum} %{A}-\
-%{A:windows.sh -T $groupNum:} ${title}  %{A}"
+%{A1:windows.sh -T $groupNum:}\
+%{A3:windows.sh -h $groupNum:}\
+  #${groupNum} - ${title}  \
+%{A}%{A}"
     } || {
         continue
     }
@@ -73,4 +78,4 @@ for group in $GROUPSDIR/group.?; do
     }
 done
 
-echo "${stringOut}"
+printf '%s\n' "${stringOut}"
