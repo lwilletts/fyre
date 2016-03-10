a27633f9421ebcf793a5b9e5507ded21aae0d45a
Laurence Willetts <me@wildefyr.net>
2016-03-10 20:04:56 +0000

    screens.sh: try 3

--- screens.sh
+++ screens.sh
@@ -9,10 +9,11 @@
 test -f "$SCREENS" && rm -f "$SCREENS"
 
 xrandr | grep -w 'connected' | while read -r connected; do
-    X="$(printf '%s\n' $connected | cut -d\  -f 3 | cut -d'+' -f 2)"
-    Y="$(printf '%s\n' $connected | cut -d\  -f 3 | cut -d'+' -f 3)"
-    W="$(printf '%s\n' $connected | cut -d\  -f 3 | cut -d'x' -f 1)"
-    H="$(printf '%s\n' $connected | cut -d\  -f 3 | cut -d'x' -f 2 | cut -d'+' -f 1)"
-    SCREEN=$(printf '%s\n' $connected | cut -d\ -f 1)
-    printf '%s\n' "$ $X $Y $W $H" >> $SCREENS
+    geometry=$(printf '%s\n' "$connected" | cut -d\  -f 3)
+    X="$(printf '%s\n' "$geometry" | cut -d'+' -f 2)"
+    Y="$(printf '%s\n' "$geometry" | cut -d'+' -f 3)"
+    W="$(printf '%s\n' "$geometry" | cut -d'x' -f 1)"
+    H="$(printf '%s\n' "$geometry" | cut -d'x' -f 2 | cut -d'+' -f 1)"
+    SCREEN=$(printf '%s\n' "$connected" | cut -d\  -f 1)
+    printf '%s\n' "$SCREEN $X $Y $W $H" >> $SCREENS
 done
