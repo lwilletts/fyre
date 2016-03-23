8edc86adf9bd09fb32790677e1c29a0433309015
Laurence Willetts <me@wildefyr.net>
2016-03-23 02:58:42 +0000

    fullscreen.sh: small tweaks

--- fullscreen.sh
+++ fullscreen.sh
@@ -3,43 +3,38 @@
 # wildefyr & z3bra - 2016 (c) wtfpl
 # fullscreen without borders, remembers previous window geometry
 
-ARGS="$@"
-
 usage() {
-    cat << EOF
+    cat >&2 << EOF
 Usage: $(basename $0) [wid]
 EOF
 
-    test $# -eq 0 || exit $1
+    test "$#" -eq 0 || exit $1
 }
 
-main() {
-    . fyrerc.sh
+. fyrerc.sh
 
-    case $1 in
-        0x*) wid=$1  ;;
-        *)   usage 0 ;;
-    esac
+wid=$PFW
 
-    test -f "$FSFILE" && {
-        test "$PFW" = "$wid" && {
-            setborder.sh active $wid
-            wtp $(cat $FSFILE)
-            rm $FSFILE
-        }
-        test "$PFW" != "$wid" && {
-            setborder.sh active $wid
-            setborder.sh inactive $PFW
-            wtp $(cat $FSFILE)
-            rm $FSFILE
-            $(basename $0) $wid
-        }
-        exit
+case $1 in
+    0x*) wid=$1 ;;
+esac
+
+test -f "$FSFILE" && {
+    test "$PFW" = "$wid" && {
+        setborder.sh active $wid
+        wtp $(cat $FSFILE)
+        rm $FSFILE
     }
-
-    setborder.sh none $wid
-    wattr xywhi $wid > $FSFILE
-    wtp 0 0 $SW $SH $wid
+    test "$PFW" != "$wid" && {
+        setborder.sh active $wid
+        setborder.sh inactive $PFW
+        wtp $(cat $FSFILE)
+        rm $FSFILE
+        $(basename $0) $wid
+    }
+    exit
 }
 
-main $ARGS
+setborder.sh none $wid
+wattr xywhi $wid > $FSFILE
+wtp 0 0 $SW $SH $wid
