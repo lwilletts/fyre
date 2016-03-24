71ba23565e14346bf697fd52cfa1e619bf415b93
Laurence Willetts <me@wildefyr.net>
2016-03-24 00:09:38 +0000

    move.sh: remove unneeded border abjustments

--- move.sh
+++ move.sh
@@ -17,13 +17,7 @@ EOF
 }
 
 move_left() {
-    X=$(wattr x $PFW)
-    Y=$(wattr y $PFW)
-    test $W -gt $minW && {
-        X=$((X - minW - IGAP - BW))
-    } || {
-        X=$((X - minW - IGAP))
-    }
+    X=$((X - minW - IGAP))
     test $X -le $XGAP && {
         snap.sh h
         exit
@@ -31,39 +25,23 @@ move_left() {
 }
 
 move_right() {
-    X=$(wattr x $PFW)
-    Y=$(wattr y $PFW)
-    test $W -gt $minW && {
-        X=$((X + minW + IGAP + BW))
-    } || {
-        X=$((X + minW + IGAP))
-    }
-    test $((X + W)) -gt $SW && {
+    X=$((X + minW + IGAP))
+    test $((X + W)) -gt $eSW && {
         snap.sh l
         exit
     }
 }
 
 move_down() {
-    test $H -gt $minH && {
-        Y=$((Y + minH + VGAP + BW))
-    } || {
-        Y=$((Y + minH + VGAP))
-    }
-    test $((Y + H)) -gt $SH && {
+    Y=$((Y + minH + VGAP))
+    test $((Y + H)) -gt $eSH && {
         snap.sh j
         exit
     }
 }
 
 move_up() {
-    X=$(wattr x $PFW)
-    Y=$(wattr y $PFW)
-    test $H -gt $minH && {
-        Y=$((Y - minH - VGAP - BW))
-    } || {
-        Y=$((Y - minH - VGAP))
-    }
+    Y=$((Y - minH - VGAP))
     test $Y -lt $TGAP && {
         snap.sh k
         exit
@@ -84,9 +62,6 @@ wid=$PFW
 case $2 in
     0x*) wid=$2 ;;
 esac
-
-SW=$eSW
-SH=$eSH
 
 case $1 in
     h|left)  move_left  ;;
