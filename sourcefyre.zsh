6147304740509f1eea33bbef73fd0873510834c4
Laurence Willetts <me@wildefyr.net>
2016-04-12 01:57:21 +0100

    oops

--- sourcefyre.zsh
+++ sourcefyre.zsh
@@ -1,9 +1,11 @@
 type fyrerc 2>&1 > /dev/null && {
-    . fyrerc
+    test ! -z "$DISPLAY" && {
+        . fyrerc
 
-    alias quit="power --exit"
-    alias lock="power --lock"
-    alias reboot="power --reboot"
-    alias poweroff="power --power"
-    alias editfyre="$EDITOR $FYREDIR/config"
+        alias quit="power --exit"
+        alias lock="power --lock"
+        alias reboot="power --reboot"
+        alias poweroff="power --power"
+        alias editfyre="$EDITOR $FYREDIR/config"
+    }
 }
